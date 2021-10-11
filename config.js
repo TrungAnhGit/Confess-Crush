f194ccf681a02b5fd98744a2dea97931944e7408
TrungAnhGit <91443488+TrungAnhGit@users.noreply.github.com>
2021-10-11 10:38:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "From me to Truc ❤️💖💕",
+    introTitle: 'Gia Trúc',
+    introDesc: `Mình đã thích bạn từ giữa năm lớp 6 , cho đến bây giờ mình vẫn mãi tương tư về bạn `,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnYes: 'aww bạn thật xinh đẹp, khiến mình không thể ngừng nghĩ về bạn <33',
+    btnNo: 'Cảm ơn Trúc đã cho mình một cảm giác thật đặc biệt !',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại khiến mình yêu bạn đến thế <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: ' 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Nhắn địa chỉ mình gửi quà nha😘 .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
